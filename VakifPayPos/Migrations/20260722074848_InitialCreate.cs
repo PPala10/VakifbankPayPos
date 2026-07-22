2ecfc6b08cb99447e962850709a9b136fd056178
PPala10 <ppala23@ku.edu.tr>
2026-07-22 11:06:31 +0300

    Initial Create and File Organization

---
+++ VakifPayPos/Migrations/20260722074848_InitialCreate.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VakifPayPos.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    customerID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    customerType = table.Column<string>(type: "text", nullable: false),
+                    companyName = table.Column<string>(type: "text", nullable: false),
+                    taxNumber = table.Column<string>(type: "text", nullable: false),
+                    taxOffice = table.Column<string>(type: "text", nullable: false),
+                    managerName = table.Column<string>(type: "text", nullable: false),
+                    managerTCKN = table.Column<long>(type: "bigint", nullable: false),
+                    phone = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    webURL = table.Column<string>(type: "text", nullable: false),
+                    city = table.Column<string>(type: "text", nullable: false),
+                    state = table.Column<string>(type: "text", nullable: false),
+                    IBAN = table.Column<string>(type: "text", nullable: false),
+                    address = table.Column<string>(type: "text", nullable: false),
+                    longitute = table.Column<decimal>(type: "numeric", nullable: false),
+                    latitude = table.Column<decimal>(type: "numeric", nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    businessType = table.Column<string>(type: "text", nullable: false),
+                    monthlySalary = table.Column<string>(type: "text", nullable: false),
+                    createDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updateDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.customerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pos",
+                columns: table => new
+                {
+                    posID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    posName = table.Column<string>(type: "text", nullable: false),
+                    posType = table.Column<string>(type: "text", nullable: false),
+                    priceType = table.Column<string>(type: "text", nullable: false),
+                    price = table.Column<int>(type: "integer", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    valorType = table.Column<string>(type: "text", nullable: false),
+                    commisionRate = table.Column<decimal>(type: "numeric", nullable: false),
+                    isPurchase = table.Column<bool>(type: "boolean", nullable: false),
+                    isActive = table.Column<bool>(type: "boolean", nullable: false),
+                    createDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updateDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pos", x => x.posID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerDocuments",
+                columns: table => new
+                {
+                    documentId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    customerId = table.Column<int>(type: "integer", nullable: false),
+                    documentType = table.Column<string>(type: "text", nullable: false),
+                    filePath = table.Column<string>(type: "text", nullable: false),
+                    isApproved = table.Column<bool>(type: "boolean", nullable: false),
+                    createDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updateDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerDocuments", x => x.documentId);
+                    table.ForeignKey(
+                        name: "FK_CustomerDocuments_Customers_customerId",
+                        column: x => x.customerId,
+                        principalTable: "Customers",
+                        principalColumn: "customerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    orderId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    customerId = table.Column<int>(type: "integer", nullable: false),
+                    totalAmount = table.Column<int>(type: "integer", nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    createDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updateDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.orderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_customerId",
+                        column: x => x.customerId,
+                        principalTable: "Customers",
+                        principalColumn: "customerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                columns: table => new
+                {
+                    orderItemId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    orderId = table.Column<int>(type: "integer", nullable: false),
+                    productId = table.Column<int>(type: "integer", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    unitPrice = table.Column<int>(type: "integer", nullable: false),
+                    commisionRate = table.Column<decimal>(type: "numeric", nullable: false),
+                    createDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updateDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.orderItemId);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Orders_orderId",
+                        column: x => x.orderId,
+                        principalTable: "Orders",
+                        principalColumn: "orderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Pos_productId",
+                        column: x => x.productId,
+                        principalTable: "Pos",
+                        principalColumn: "posID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    paymentId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    orderId = table.Column<int>(type: "integer", nullable: false),
+                    cardNumber = table.Column<string>(type: "text", nullable: false),
+                    cardHolderName = table.Column<string>(type: "text", nullable: false),
+                    cardExpirationDate = table.Column<string>(type: "text", nullable: false),
+                    CVC = table.Column<string>(type: "text", nullable: false),
+                    PaymentStatus = table.Column<string>(type: "text", nullable: false),
+                    PaidDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.paymentId);
+                    table.ForeignKey(
+                        name: "FK_Payments_Orders_orderId",
+                        column: x => x.orderId,
+                        principalTable: "Orders",
+                        principalColumn: "orderId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerDocuments_customerId",
+                table: "CustomerDocuments",
+                column: "customerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_orderId",
+                table: "OrderItems",
+                column: "orderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_productId",
+                table: "OrderItems",
+                column: "productId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_customerId",
+                table: "Orders",
+                column: "customerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_orderId",
+                table: "Payments",
+                column: "orderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerDocuments");
+
+            migrationBuilder.DropTable(
+                name: "OrderItems");
+
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropTable(
+                name: "Pos");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
